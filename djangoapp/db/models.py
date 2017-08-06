89709c794cbad54505d5f324f38611c3f3048d99
Alessandro Sanchez <amsan7@gmail.com>
2017-08-06 15:36:10 -0700

    added new fields to token / utterances table, added indexes to child age

--- djangoapp/db/models.py
+++ djangoapp/db/models.py
@@ -22,42 +22,6 @@ class Collection(Model):
 class Corpus(Model):
     name = CharField(max_length=255, blank=True, default=None, null=True) # simple name
     collection = ForeignKey(Collection, blank=True, null=True, default=None)
-    # title = CharField(max_length=255, blank=True, default=None, null=True)  # longer name found in metadata
-    # creator = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # subject = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # subject_olac_linguistic_field = CharField(db_column='subject.olac.linguistic.field', max_length=255, blank=True, default=None, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    # subject_olac_language = CharField(db_column='subject.olac.language', max_length=255, blank=True, default=None, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    # subject_childes_participant_age = CharField(db_column='Subject.childes.Participant.age', max_length=255, blank=True, default=None, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    # description = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # publisher = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # contributor = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # date = DateField(max_length=255, blank=True, default=datetime.now(), null=True)
-    # type = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # type_olaclinguistic_type = CharField(db_column='type.olaclinguistic-type', max_length=255, blank=True, default=None, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    # type_olacdiscourse_type = CharField(db_column='type.olacdiscourse-type', max_length=255, blank=True, default=None, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    # format = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # identifier = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # language = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # relation = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # coverage = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # rights = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # imdi_genre = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # imdi_interactivity = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # imdi_planningtype = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # imdi_involvement = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # imdi_socialcontext = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # imdi_eventstructure = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # imdi_task = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # imdi_modalities = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # imdi_subject = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # imdi_ethnicgroup = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # imdi_recordingconditions = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # imdi_accessavailability = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # imdi_continent = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # imdi_country = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # imdi_projectdescription = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # imdi_mediafiledescription = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
-    # imdi_writtenresourcesubtype = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
 
     class Meta:
         app_label = 'db'
@@ -68,9 +32,7 @@ class Participant(Model):
     code = CharField(max_length=255, blank=True, default=None, null=True)
     name = CharField(max_length=255, blank=True, default=None, null=True)  # Field name made lowercase.
     role = CharField(max_length=255, blank=True, default=None, null=True)
-    # transcript = ForeignKey(Transcript, blank=True, null=True, default=None)
     corpus = ForeignKey(Corpus, blank=True, null=True, default=None)
-    # age = FloatField(blank=True, null=True, default=None)
     min_age = FloatField(blank=True, null=True, default=None)
     max_age = FloatField(blank=True, null=True, default=None)
     language = CharField(max_length=255, blank=True, default=None, null=True)
@@ -89,26 +51,10 @@ class Participant(Model):
 class Transcript(Model):
     corpus = ForeignKey(Corpus, blank=True, null=True, default=None)
     languages = CharField(max_length=255, blank=True, default=None, null=True)
-    # exceptions = CharField(max_length=255, blank=True, default=None, null=True)
-    # interaction_type = CharField(max_length=255, blank=True, default=None, null=True)
-    # location = CharField(max_length=255, blank=True, default=None, null=True)
-    # number = CharField(max_length=255, blank=True, default=None, null=True)
-    # recording_quality = CharField(max_length=255, blank=True, default=None, null=True)
-    # room_layout = CharField(max_length=255, blank=True, default=None, null=True)
-    # tape_location = CharField(max_length=255, blank=True, default=None, null=True)
-    # time_duration = CharField(max_length=255, blank=True, default=None, null=True)
-    # time_start = CharField(max_length=255, blank=True, default=None, null=True)
-    # transcription = CharField(max_length=255, blank=True, default=None, null=True)
-    # warning = CharField(max_length=255, blank=True, default=None, null=True)
-    # activities = CharField(max_length=255, blank=True, default=None, null=True)
-    # comment = CharField(max_length=255, blank=True, default=None, null=True)
     date = DateField(max_length=255, blank=True, default=datetime.now, null=True)
-    # new_episode = CharField(max_length=255, blank=True, default=None, null=True)
-    # new_language = CharField(max_length=255, blank=True, default=None, null=True)
-    # page = CharField(max_length=255, blank=True, default=None, null=True)
-    # situation = CharField(max_length=255, blank=True, default=None, null=True)
     filename = CharField(max_length=255, blank=True, default=None, null=True)
     target_child = ForeignKey(Participant, blank=True, null=True, default=None)
+    target_child_name = CharField(max_length=255, blank=True, default=None, null=True)
     target_child_age = FloatField(blank=True, null=True, default=None)
 
     class Meta:
@@ -130,20 +76,15 @@ class Utterance(Model):
     speaker_name = CharField(max_length=255, blank=True, default=None, null=True)
     speaker_age = FloatField(blank=True, null=True, default=None)
     speaker_role = CharField(max_length=255, blank=True, default=None, null=True)
+    speaker_sex = CharField(max_length=255, blank=True, default=None, null=True)
+    target_child = ForeignKey(Participant, blank=True, null=True, default=None, related_name="all_related_utterances")
+    target_child_name = CharField(max_length=255, blank=True, default=None, null=True)
+    target_child_age = FloatField(db_index=True, blank=True, null=True, default=None)
+    target_child_sex = CharField(max_length=255, blank=True, default=None, null=True)
 
     class Meta:
         app_label = 'db'
         db_table = 'utterance'
-
-
-# class DependentTier(Model):
-#     code = CharField(max_length=255, blank=True, default=None, null=True)
-#     line = CharField(max_length=255, blank=True, default=None, null=True)
-#     utterance = ForeignKey(Utterance, blank=True, null=True, default=None)
-#
-#     class Meta:
-#         app_label = 'db'
-#         db_table = 'dependent_tier'
 
 
 class Token(Model):
@@ -161,11 +102,12 @@ class Token(Model):
     speaker_name = CharField(max_length=255, blank=True, default=None, null=True)
     speaker_age = FloatField(blank=True, null=True, default=None)
     speaker_role = CharField(max_length=255, blank=True, default=None, null=True)
+    speaker_sex = CharField(max_length=255, blank=True, default=None, null=True)
+    target_child = ForeignKey(Participant, blank=True, null=True, default=None, related_name="all_related_tokens")
+    target_child_name = CharField(max_length=255, blank=True, default=None, null=True)
+    target_child_age = FloatField(db_index=True, blank=True, null=True, default=None)
+    target_child_sex = CharField(max_length=255, blank=True, default=None, null=True)
 
     class Meta:
         app_label = 'db'
         db_table = 'token'
-
-
-
-
