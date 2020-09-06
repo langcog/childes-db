e8fa50033f9c259c70072f93f3c9816a9a98923b
Sathvik Nair <sathviknair@berkeley.edu>
2020-09-05 20:05:29 -0700

    encoding mismatch between data from XML and SQL query

--- tests/check_phonology.py
+++ tests/check_phonology.py
@@ -1,6 +1,7 @@
 import os
 import MySQLdb
 import pandas as pd
+from nltk.corpus.reader.xmldocs import ElementTree
 
 params = {
 	"host":"ec2-54-68-171-132.us-west-2.compute.amazonaws.com",
@@ -31,32 +32,51 @@ def connect_to_childes(db_version = 'current', db_args = None):
     return childes_con
 
 
-def parse_xml(row, u_index):
+def parse_xml(xml_path, u_index):
     """
     Read XML file from the row, and return the actual IPA and model IPA
+    Args: 
+        xml_path: filename in childes-db, path to XML with this utterance
+        u_index: utterance_order in childes-db, represents the index of this utterance
+    Returns:
+        two strings which should correspond to actual_phonology and model_phonology for this utterance 
     """
+    NS = 'http://www.talkbank.org/ns/talkbank' #resolving namespace issue (from db/childes.py)
+    fileid = os.path.join("../../../../", "shared_hd1", "childes-db-xml", xml_path) #FIXME: change to absolute path
+    tree = ElementTree.parse(fileid)
+    xmldoc = tree.getroot()
 
+    uid = 'u' + str(u_index - 1)
+    './/{%s}u[@uID="u2"]'
+    xmlsent = xmldoc.find('.//{%s}u' % NS + '[@uID="' + uid + '"]')
+    actual_words = xmlsent.findall('.//{%s}pg/{%s}actual/{%s}pw' % (NS, NS, NS))
+    model_words = xmlsent.findall('.//{%s}pg/{%s}model/{%s}pw' % (NS, NS, NS))
+
+    actual_pho =  [''.join([x.text for x in y.findall('{%s}ph' % NS)]) for y in actual_words]
+    model_pho =  [''.join([x.text for x in y.findall('{%s}ph' % NS)]) for y in model_words]
+    return ' '.join(actual_pho), ' '.join(model_pho) 
+    
 def log_error(row, exception):
     """
     Save error in logs
     """
+    pass
 db = connect_to_childes(db_args = params)
 #Pull data from the utterances table and join it with the transcripts table to get the path to the right file
 query = "SELECT utterance.id, gloss, utterance.language, actual_phonology, model_phonology, utterance_order, filename \
 FROM utterance JOIN transcript ON utterance.transcript_id = transcript.id \
-WHERE actual_phonology <> '' AND model_phonology <> ''"
+WHERE actual_phonology <> '' AND model_phonology <> '' AND filename = 'Eng-NA/Penney/Narrative/D43.xml' LIMIT 1"
 
 print("Running query to childes-db")
 df = pd.read_sql(query, db)
 db.close()
 
-df = df.sample(sample_size)
+#df = df.sample(sample_size)
 
-for r in df.iterrows():
-    parsed_actual, parsed_model = parse_xml(row)
+for _, r in df.iterrows():
+    parsed_actual, parsed_model = parse_xml(r['filename'], r['utterance_order'])
     db_actual, db_model = r['actual_phonology'], r['model_phonology']
-    try:
-        assert parsed_actual == db_actual
-        assert parsed_model == db_model
-    except:
-        log_error(row, exception)
+    print(parsed_actual.encode('utf-8'), parsed_model.encode('utf-8'))
+    print(db_actual, db_model)
+    assert parsed_actual == db_actual
+    assert parsed_model == db_model
