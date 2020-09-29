8dd88b52685395435d55629e0a48a803ea706614
Sathvik Nair <sathviknair@berkeley.edu>
2020-09-28 22:53:11 -0700

    Add name = main for testing (some vals imported)

--- compare_refactor.py
+++ compare_refactor.py
@@ -81,35 +81,36 @@ def compare_transcripts(ec2_con, local_con, ec2_id, local_id, token_fmt, utt_fmt
         mismatched_tokens.append({'ec2_id': ec2_id, 'local_id': local_id})
     
 
-ec2_con = connect_to_childes(db_args = ec2_params)
-local_con = connect_to_childes(db_args = local_params)
+if __name__ == "__main__":
+    ec2_con = connect_to_childes(db_args = ec2_params)
+    local_con = connect_to_childes(db_args = local_params)
 
-transcript_query = "SELECT id, filename FROM transcript"
-ec2_transcripts = pd.read_sql(transcript_query, ec2_con)
-local_transcripts = pd.read_sql(transcript_query, local_con)
+    transcript_query = "SELECT id, filename FROM transcript"
+    ec2_transcripts = pd.read_sql(transcript_query, ec2_con)
+    local_transcripts = pd.read_sql(transcript_query, local_con)
 
-#Output should be a DF with local transcript ID, EC2 transcript ID, and filename.
-id_map = local_transcripts.merge(ec2_transcripts, on = 'filename', how = 'inner', suffixes = ['_local', '_ec2'])
+    #Output should be a DF with local transcript ID, EC2 transcript ID, and filename.
+    id_map = local_transcripts.merge(ec2_transcripts, on = 'filename', how = 'inner', suffixes = ['_local', '_ec2'])
 
-"""
-In order to see if the version on Chompsky is consistent with EC2, we need to have a consistent set of columns.
-Some additions have been made to the current version of Childes DB (i.e. dependency parse columns), and IDs may be 
-inconsistent across machines.
-"""
-strip_id_cols = lambda cols: [c for c in cols if not c.endswith('id')] #Getting rid of the 
-ec2_token_cols = strip_id_cols(pd.read_sql("SELECT * FROM token WHERE 1 = 0", ec2_con).columns)
-ec2_utt_cols = strip_id_cols(pd.read_sql("SELECT * FROM utterance WHERE 1 = 0", ec2_con).columns)
+    """
+    In order to see if the version on Chompsky is consistent with EC2, we need to have a consistent set of columns.
+    Some additions have been made to the current version of Childes DB (i.e. dependency parse columns), and IDs may be 
+    inconsistent across machines.
+    """
+    strip_id_cols = lambda cols: [c for c in cols if not c.endswith('id')] #Getting rid of the 
+    ec2_token_cols = strip_id_cols(pd.read_sql("SELECT * FROM token WHERE 1 = 0", ec2_con).columns)
+    ec2_utt_cols = strip_id_cols(pd.read_sql("SELECT * FROM utterance WHERE 1 = 0", ec2_con).columns)
 
-for index, row in tqdm(id_map.iterrows(), total = id_map.shape[0]):
-    local_tid, ec2_tid = row['id_local'], row['id_ec2']
-    compare_transcripts(ec2_con, local_con, ec2_tid, local_tid, ec2_token_cols, ec2_utt_cols)
+    for index, row in tqdm(id_map.iterrows(), total = id_map.shape[0]):
+        local_tid, ec2_tid = row['id_local'], row['id_ec2']
+        compare_transcripts(ec2_con, local_con, ec2_tid, local_tid, ec2_token_cols, ec2_utt_cols)
 
-    print("Token-Level: %s of %s transcripts match" % (passed_token, index + 1))
-    print("Utterance-Level: %s of %s transcripts match" % (passed_utt, index + 1))
+        print("Token-Level: %s of %s transcripts match" % (passed_token, index + 1))
+        print("Utterance-Level: %s of %s transcripts match" % (passed_utt, index + 1))
 
-    if index % 10 == 0: #logging the IDs of transcripts that didn't match on EC2 and Chompsky
-        pd.DataFrame(mismatched_tokens).to_csv('token_errors.log', index = False)
-        pd.DataFrame(mismatched_utts).to_csv('utt_errors.log', index = False)
+        if index % 100 == 0: #logging the IDs of transcripts that didn't match on EC2 and Chompsky
+            pd.DataFrame(mismatched_tokens).to_csv('token_errors.log', index = False)
+            pd.DataFrame(mismatched_utts).to_csv('utt_errors.log', index = False)
 
-pd.DataFrame(mismatched_tokens).to_csv('token_errors.log', index = False)
-pd.DataFrame(mismatched_utts).to_csv('utt_errors.log', index = False)
+    pd.DataFrame(mismatched_tokens).to_csv('token_errors.log', index = False)
+    pd.DataFrame(mismatched_utts).to_csv('utt_errors.log', index = False)
