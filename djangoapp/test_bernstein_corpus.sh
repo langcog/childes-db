968d374e84dffe0829ec50f49d471b97ecb79dd1
Sathvik Nair <sathviknair@berkeley.edu>
2021-03-20 23:49:04 -0700

    test_single_corpus as mgmt command, write to single db

--- djangoapp/test_bernstein_corpus.sh
+++ djangoapp/test_bernstein_corpus.sh
@@ -1,10 +1,10 @@
 source ~/.profile
 echo "Creating new database..."
 cd static
-cat new_dev_db_test2.sql | mysql -u root -p"$ROOT_PASS"
+cat new_corpus_db.sql | mysql -u root -p"$ROOT_PASS"
 echo "Enforcing schema..."
 cd ../
 python3 manage.py migrate db
 echo "Populating....."
-python3 manage.py test_single_corpus --data_source CHILDES --collection_root /shared_hd2/childes-db/2020.1/candidate/childes.talkbank.org/data-xml --collection Eng-NA  --corpus Bernstein
+python3 manage.py test_single_corpus --data_source CHILDES --collection_root /shared_hd2/childes-db/2020.1/candidate/childes.talkbank.org/data-xml --selected_collection Eng-NA  --corpus Bernstein
 #python3 manage.py populate_db --data_source PhonBank --collection_root /shared_hd2/childes-db-xml/2020.1/candidate/phonbank.talkbank.org/data-xml --collection Eng-NA
