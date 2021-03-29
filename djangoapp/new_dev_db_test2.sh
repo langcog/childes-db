bc316c186a1d31143b5c9a3553c33cd89b65ad20
Sathvik Nair <sathviknair@berkeley.edu>
2021-03-28 21:21:13 -0700

    Correct filepaths

--- djangoapp/new_dev_db_test2.sh
+++ djangoapp/new_dev_db_test2.sh
@@ -6,5 +6,5 @@ echo "Enforcing schema..."
 cd ../
 python3 manage.py migrate db
 echo "Populating....."
-python3 manage.py populate_db --data_source CHILDES --collection_root /shared_hd2/childes-db-xml/2020.1/candidate/childes.talkbank.org/data-xml --collection Eng-NA
+python3 manage.py populate_db --data_source CHILDES --collection_root /shared_hd2/childes-db/2020.1/candidate/childes.talkbank.org/data-xml
 #python3 manage.py populate_db --data_source PhonBank --collection_root /shared_hd2/childes-db-xml/2020.1/candidate/phonbank.talkbank.org/data-xml --collection Eng-NA
